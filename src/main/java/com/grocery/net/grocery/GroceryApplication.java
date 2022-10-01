1d7b9257ba8dd80d70c7c881ef101ac6017996f3
unknown <shilpamohanan97@gmail.com>
2022-10-01 12:49:01 +0530

    Initial Project Creation

---
+++ src/main/java/com/grocery/net/grocery/GroceryApplication.java
@@ -0,0 +1,13 @@
+package com.grocery.net.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryApplication.class, args);
+	}
+
+}
